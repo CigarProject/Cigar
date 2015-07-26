b522162cfbaba81cdd676cc35dfe82e9417a1eb1
m-byte <m-byte@users.noreply.github.com>
2015-07-26 08:31:22 +0200

    Update index.js
    Ogar -> Cigar

--- server/web/routes/index.js
+++ server/web/routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('game', {title: 'Ogar', game: true});
+    res.render('game', {title: 'Cigar', game: true});
 });
 
 router.get('/gallery', function (req, res, next) {
-    res.render('gallery', {title: 'Ogar skins', gallery: true});
+    res.render('gallery', {title: 'Cigar skins', gallery: true});
 });
 
 module.exports = router;
