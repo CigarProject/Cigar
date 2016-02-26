71b0c3447ee6b9e1e9eeee0e4b07077dece6a496
m-byte <m-byte@users.noreply.github.com>
2016-02-26 16:14:59 +0100

    Fix #130 (1/2)
    Use nodejs instead of node if Debian is detected.

--- start_gameserver.sh
+++ start_gameserver.sh
@@ -1,9 +1,19 @@
-#/bin/bash
+#!/bin/bash
+node="node"
+which apt-get > /dev/null
+if [ "$?" -eq "0" ]; then
+	node=$(which nodejs)
+	if [ "$?" -ne "0" ]; then
+		echo "Could not find nodejs! Please run \"apt-get install nodejs\" to install it."
+		exit 1
+	fi
+	echo "Debian detected. Using nodejs instead of node..."
+fi
 echo [BATCH] Starting Server without MasterServer
 while true
 do
 	clear
-	node server/index.js
+	$node server/index.js
 	echo [BATCH] Server Shutdown, waiting 15 seconds before a restart.
 	sleep 15
 done
