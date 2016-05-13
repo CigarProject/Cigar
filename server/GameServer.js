b85377fab54858724071f72dacfcb54dd0b5ccea
ItzLevvie <ItzLevvie@users.noreply.github.com>
2016-05-13 19:07:19 +0100

    Update GameServer.js

--- server/GameServer.js
+++ server/GameServer.js
@@ -1084,29 +1084,15 @@ GameServer.prototype.getStats = function() {
 
 // Custom prototype functions
 WebSocket.prototype.sendPacket = function(packet) {
-    function getBuf(data) {
-        var array = new Uint8Array(data.buffer || data);
-        var l = data.byteLength || data.length;
-        var o = data.byteOffset || 0;
-        var buffer = new Buffer(l);
-
-        for (var i = 0; i < l; i++) {
-            buffer[i] = array[o + i];
+    if (this.readyState == WebSocket.OPEN && (this._socket.bufferSize == 0) && packet.build) {
+        try {
+            this.send(packet.build(), {binary: true});
+        } catch (e) {
+            console.log("[Error] "+e);
+            this.emit('close');
+            this.removeAllListeners();
         }
-
-        return buffer;
-    }
-
-    //if (this.readyState == WebSocket.OPEN && (this._socket.bufferSize == 0) && packet.build) {
-    if (this.readyState == WebSocket.OPEN && packet.build) {
-        var buf = packet.build();
-        this.send(getBuf(buf), {
-            binary: true
-        });
-    } else if (!packet.build) {
-        // Do nothing
     } else {
-        this.readyState = WebSocket.CLOSED;
         this.emit('close');
         this.removeAllListeners();
     }
