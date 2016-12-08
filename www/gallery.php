19c318e590e5f8bc05bf34bba115ed25f0e25450
HAlexTM <halexercraft@gmail.com>
2016-12-08 14:11:46 +0100

    Fix for subfolders images And remove .png from skin name

--- www/gallery.php
+++ www/gallery.php
@@ -57,8 +57,8 @@
                         if (!in_array($curimg, $ignore) && strtolower(pathinfo($curimg, PATHINFO_EXTENSION)) == "png") {
                 ?>
                 <li>
-                    <div class="circular" style='background-image: url("/<?php echo $dirname.$curimg ?>")'></div>
-                    <h4 style="text-align: center"><?php echo $curimg ?></h4>
+                    <div class="circular" style='background-image: url("./<?php echo $dirname.$curimg ?>")'></div>
+                    <h4 style="text-align: center"><?php echo pathinfo($curimg, PATHINFO_FILENAME); ?></h4>
                 </li>
                 <?php
                         }
