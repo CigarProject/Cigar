547a41f5b5d0d25237b994347fbee36254f5372d
Luka967 <luka.djokovic2@gmail.com>
2017-01-02 18:27:47 +0100

    Replace decodeURIComponent with plain code

--- www/assets/js/reader.js
+++ www/assets/js/reader.js
@@ -32,6 +32,39 @@ Reader.prototype = {
     getStringUTF8: function() {
         var bytes = [], b;
         while ((b = this.view.getUint8(this._o++)) !== 0) bytes.push(b);
-        return decodeURIComponent(escape(String.fromCharCode.apply(bytes)));
+
+        // Thanks to Albert from StackOverflow
+        // (Semi-) Original can be found at http://stackoverflow.com/questions/17191945
+        var out, i, len, c;
+        var char2, char3;
+
+        out = "";
+        len = bytes.length;
+        i = 0;
+        for ( ; i < len; ) {
+            c = bytes[i++];
+            switch(c >> 4)
+            {
+                case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+                    // 0xxxxxxx
+                    out += String.fromCharCode(c);
+                    break;
+                case 12: case 13:
+                    // 110x xxxx   10xx xxxx
+                    char2 = bytes[i++];
+                    out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+                    break;
+                case 14:
+                    // 1110 xxxx  10xx xxxx  10xx xxxx
+                    char2 = bytes[i++];
+                    char3 = bytes[i++];
+                    out += String.fromCharCode(((c & 0x0F) << 12)
+                        ((char2 & 0x3F) << 6) |
+                        ((char3 & 0x3F) << 0));
+                break;
+            }
+        }
+
+        return out;
     }
 };
