e7f9bf1cace20bced03b1a83a4341f3f8f92717a
Luka967 <luka.djokovic2@gmail.com>
2017-01-05 00:17:55 +0100

    Log verbosity, LOAD_START measuring from here

--- www/assets/js/log.js
+++ www/assets/js/log.js
@@ -1,21 +1,27 @@
-// Developed by the CigarProject //
+log = {
+    // 0 - Only info
+    // 1 - Info & warns
+    // 2 - Info, warns & errors
+    // 3 - All
+    VERBOSITY: 3,
 
-var log = {
-
-	info: function(str) {
-		console.debug("[INFO]", str);
+    info: function(str) {
+		console.info("[INFO]", str);
 	},
 
 	warn: function(str) {
+        if (log.VERBOSITY < 1) return;
 		console.warn("[WARN]", str);
 	},
 
 	err: function(str) {
-		console.error("[ERROR] ", str);
+        if (log.VERBOSITY < 2) return;
+		console.error("[ERROR]", str);
 	},
 
 	debug: function(str) {
-		console.info("[DEBUG] ", str);
+        if (log.VERBOSITY < 3) return;
+		console.debug("[DEBUG]", str);
 	}
-
 }
+var LOAD_START = new Date();
