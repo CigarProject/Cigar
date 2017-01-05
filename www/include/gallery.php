9ef9e112517524ef5002463242aa6e9354078650
Luka967 <luka.djokovic2@gmail.com>
2017-01-05 19:17:42 +0100

    Fix PHP errors

--- www/include/gallery.php
+++ www/include/gallery.php
@@ -2,19 +2,24 @@
 <div class="row center">
     <ul>
         <?php
-            $dirname = "../skins/";
-            $images = scandir($dirname);
-            $ignore = array(".", "..");
+            # Skin directory relative to include/gallery.php (this file)
+            $skindir = "../skins/";
+
+            # Skin directory relative to index.html
+            $skindirhtml = "./skins/";
+
+            $images = scandir($skindir);
+
             foreach($images as $curimg) {
-                if (!in_array($curimg, $ignore) && strtolower(pathinfo($curimg, PATHINFO_EXTENSION)) == "png") {
+                if (strtolower(pathinfo($curimg, PATHINFO_EXTENSION)) == "png") {
         ?>
-        <li class="skin" onclick="$('#nick').val($(this).find('.title').text());" data-dismiss="modal">
-            <div class="circular" style='background-image: url("./<?php echo $dirname.$curimg ?>")'></div>
+        <li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
+            <div class="circular" style='background-image: url("./<?php echo $skindirhtml.$curimg ?>")'></div>
             <h4 class="title"><?php echo pathinfo($curimg, PATHINFO_FILENAME); ?></h4>
         </li>
         <?php
                 }
-            }                 
+            }
         ?>
     </ul>
 </div>
